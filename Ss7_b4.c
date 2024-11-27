68dbad63f5717aa305a7e98c8dfac420174e06c1
AnHaiDung <anhaidung2k6@gmail.com>
2024-11-27 22:31:00 +0700

    b4

---
+++ Ss7_b4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int i,spt,a[i];
+	printf("nhap so phan tu cua mang : ");
+	scanf("%d",&spt);
+	for(int i=0;i<spt;i++){
+		printf("nhap phan tu thu %d : ",i+1);
+		scanf("%d",&a[i]);
+	}
+	return 0;
+}
